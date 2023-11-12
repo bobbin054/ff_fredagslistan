8b39295438f6965483ed8f654336503db7fc1a16
Joakim Östelid <joakim.ostelid@outlook.com>
2023-11-12 22:19:20 +0100

    Remove prettier-plugin-organize-imports and update WheelComponent in About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,46 +1,41 @@
-'use client';
-import React from 'react'
-import WheelComponent from '../_components/WheelComponent'
+"use client";
+import React from "react";
+import WheelComponent from "../_components/WheelComponent";
 
 export default function About() {
-  const segments = [
-    '1',
-    '2',
-    '3',
-    '4',
-  ]
+  const segments = ["1", "2", "3", "4"];
   const segColors = [
-    '#EE4040',
-    '#F0CF50',
-    '#815CD1',
-    '#3DA5E0',
-    '#34A24F',
-    '#F9AA1F',
-    '#EC3F3F',
-    '#FF9000',
-    '#F0CF50',
-    '#815CD1',
-    '#3DA5E0',
-    '#34A24F',
-    '#F9AA1F',
-    '#EC3F3F',
-    '#FF9000'
-  ]
-  const onFinished = (winner) => {
-    console.log(winner)
-  }
+    "#EE4040",
+    "#F0CF50",
+    "#815CD1",
+    "#3DA5E0",
+    "#34A24F",
+    "#F9AA1F",
+    "#EC3F3F",
+    "#FF9000",
+    "#F0CF50",
+    "#815CD1",
+    "#3DA5E0",
+    "#34A24F",
+    "#F9AA1F",
+    "#EC3F3F",
+    "#FF9000",
+  ];
+  const onFinished = (winner: string) => {
+    console.log(winner);
+  };
   return (
     <WheelComponent
-        segments={segments}
-        segColors={segColors}
-        onFinished={(winner) => onFinished(winner)}
-        primaryColor='gold'
-        contrastColor='black'
-        buttonText='Spin'
-        isOnlyOnce={false}
-        size={200}
-        upDuration={100}
-        downDuration={500}
-      />
-  )
+      segments={segments}
+      segColors={segColors}
+      onFinished={(winner) => onFinished(winner)}
+      primaryColor="gold"
+      contrastColor="black"
+      buttonText="Spin"
+      isOnlyOnce={false}
+      size={200}
+      upDuration={100}
+      downDuration={500}
+    />
+  );
 }
